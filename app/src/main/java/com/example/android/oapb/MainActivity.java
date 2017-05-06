56bd596b39343e1fd1e92f9e004b3b619f62aca7
121ado <121adulo@gmail.com>
2017-05-06 17:26:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/oapb/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.oapb;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
